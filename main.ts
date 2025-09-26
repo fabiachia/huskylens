b32422ba439286aa55296d2a343a4f402e7daba8
fabiachia <fabiachia@gmail.com>
2025-09-26 23:30:23 +0200

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
 basic.forever(function () {
-	
+    huskylens.request()
+    if (huskylens.isLearned(3)) {
+        basic.showIcon(IconNames.Heart)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
 })
